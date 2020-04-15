8696aaf0eb83eaa346cd643537ed8f4ec241034e
Igor <basovigorg@gmail.com>
2020-04-15 21:39:28 +0300

    feat: load protected user data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
     <h3>Dashboard</h3>
+
+    <p v-if="!inProgress">
+      Hello, <strong>{{ name }}</strong> ({{ email }})
+    </p>
+    <p v-else>
+      Loading user data...
+    </p>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      inProgress: true,
+      name: '',
+      email: ''
+    };
+  },
+  created() {
+    axios.get('http://localhost:3000/whoami').then(({ data }) => {
+      this.name = data.name;
+      this.email = data.email;
+      this.inProgress = false;
+    });
+  }
+};
 </script>
