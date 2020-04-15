f4a2d7f2e201fc8895c83a649c13bad17b236cb6
Igor <basovigorg@gmail.com>
2020-04-15 22:29:39 +0300

    feat: auto login and logout on unauthorize

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 import router from './router';
 import store from './vuex/store';
@@ -10,5 +11,24 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   router,
-  store
+  store,
+  created() {
+    const userString = localStorage.getItem('user');
+
+    if (userString) {
+      const user = JSON.parse(userString);
+      this.$store.commit('SET_USER_DATA', user);
+    }
+
+    // Logout user if HTTP 401 receieved
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout');
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
 }).$mount('#app');
